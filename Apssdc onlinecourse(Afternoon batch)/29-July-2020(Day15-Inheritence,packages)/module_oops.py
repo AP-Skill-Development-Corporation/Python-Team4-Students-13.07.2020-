6e59dfcba9f8f91989805bc516edc6acec2c8d9a
madevi431 <51846949+madevi431@users.noreply.github.com>
2020-07-30 13:38:37 +0530

    Add files via upload

---
+++ Apssdc onlinecourse(Afternoon batch)/29-July-2020(Day15-Inheritence,packages)/module_oops.py
@@ -0,0 +1,13 @@
+class ece():
+    def set_1(self):
+        print("we are in ece section 1")
+        
+class cse():
+    def set_2(self):
+        print("we are in cse section 2")
+class mech():
+    def set_3(self):
+        print("we are in mech section 3")
+        
+
+        
